8b7dddaa8e57e9cf88a8d7a4d725556c230979c3
CUBEITG\pstolka <pstolka@cubeitg.pl>
2017-09-13 15:09:54 +0200

    create svg container - appending html

--- App/js/script.js
+++ App/js/script.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
     var url = "http://localhost:3000/getcup";
 
-    var json = $.getJSON(url, function(data) {
-        console.log(data);
+    $.getJSON(url, function(data) {
+        draw(data);
     });
 });
+
+function draw(json) {
+
+    var width = 700,
+        height = 300,
+        barWidth = 50,
+        padding = 5;
+
+    // example data, not used so far.
+    var arr = [10, 8, 40, 34, 52, 45, 33, 75];
+    // 1
+    var svg = d3.select("#canvas")
+        .append("svg")
+        .attr({
+            width: width,
+            height: height,
+        })
+        .style({
+            width: width,
+            height: height,
+            border: '1px lightgray solid'
+        })
+
+    
+}
