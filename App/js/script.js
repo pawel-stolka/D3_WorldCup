9b4eb573917e15820d00283219d7376e07eebb82
CUBEITG\pstolka <pstolka@cubeitg.pl>
2017-09-13 15:18:39 +0200

    first bars - fixed height

--- App/js/script.js
+++ App/js/script.js
@@ -13,7 +13,7 @@ function draw(json) {
         barWidth = 50,
         padding = 5;
 
-    // example data, not used so far.
+    // example data, what counts is the number of elements.
     var arr = [10, 8, 40, 34, 52, 45, 33, 75];
     // 1
     var svg = d3.select("#canvas")
@@ -28,5 +28,23 @@ function draw(json) {
             border: '1px lightgray solid'
         })
 
-    
+    svg
+        .selectAll("rect")
+        .data(arr)
+        .enter()
+        .append('rect')
+        .attr({
+            width: barWidth,
+            height: 100 // for example
+        })
+        .style({
+            fill: 'red',
+            stroke: 'black',
+            'stroke-width': '2px'
+        })
+        .attr({
+            x: function(d, i) { return (i * barWidth) },
+            y: 0
+        })
+
 }
