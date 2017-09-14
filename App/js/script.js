644ce0943ed85f9a0b34ed7429313fa92335420b
CUBEITG\pstolka <pstolka@cubeitg.pl>
2017-09-14 07:41:11 +0200

    add axis, changes in html, css and js

--- App/js/script.js
+++ App/js/script.js
@@ -31,12 +31,14 @@ function draw(json) {
 
     var arr = json.teams;
     // reference to data field - points
-    var _points = "points";
-    var _color = "group";
-    var _wins = "wins";
+    var _name = "country",
+        _points = "points",
+        _color = "group",
+        _wins = "wins";
 
-    var colorScale = d3.scale.category10(); /* mamy też .category20/20b/20c */
-    /* możemy sami ustalić kolory, podając własny range */
+    var colorScale = d3.scale.category10() /* mamy też .category20/20b/20c */
+        .range(["red", "yellow", "white", "green", "orange", "brown", "cyan", "pink", "purple", "blue"]);
+
 
     var maxRadius = d3.max(arr, function(d) { return d[_wins]; });
     var radiusScale = d3.scale.linear() //.sqrt()
@@ -53,8 +55,13 @@ function draw(json) {
         .domain([0, arr.length - 1])
         .range([margin, width - margin]);
 
-    // the same with scale of X
-    svg
+    // changing svg to g.svg
+    // obszar wykresu
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin + "," + margin/2 + ")")
+        .attr("width", w);
+
+    var bubbles = g
         .selectAll("circle")
         .data(arr)
         .enter()
@@ -67,7 +74,7 @@ function draw(json) {
         .style({
             fill: function(d) { return colorScale(d[_color]); },
             stroke: 'black',
-            'stroke-width': '2px'
+            'stroke-width': '1px'
         })
         .attr({
             r: function(d) { return radiusScale(d[_wins]) },
@@ -75,4 +82,30 @@ function draw(json) {
             cy: function(d) { return height - yScale(d[_points]); } //+ padding; }
         })
 
+    bubbles
+        .append('text')
+        .attr('dx', 10)
+        .text(function(d) { return d.country; });
+
+    // - scales
+    var axisScale = d3.scale.linear()
+        .domain([maxHeight, 0])
+        .range([0, h]);
+
+    var yAxis = d3.svg.axis()
+        .scale(axisScale)
+        .orient("left")
+        .innerTickSize(-w)
+        .tickPadding(10)
+
+    var yAxisG = g.append("g") /* */
+        .attr("class", "axis y") /* */
+
+    yAxisG.call(yAxis);
+
+    // tytul osi Y
+    yAxisG.append("text")
+        .attr("transform", "translate (-40,0) rotate(-90)")
+        .attr("text-anchor", "end")
+        .text("zdobyte punkty")
 }
